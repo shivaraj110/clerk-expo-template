cd0746cbf6f99e92d9e212ae77703976c2e7037f
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-23 21:38:09 +0530

    added hook for keyboard state

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -4,32 +4,34 @@ import { getClerkInstance, useSignUp } from "@clerk/clerk-expo";
 import { useRouter } from "expo-router";
 import { useState } from "react";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import useKeyboardState from "../hook/useKeyboardState";
 
 export default function Signup() {
   const router = useRouter();
   const [form, setForm] = useState({
     name: "",
     email: "",
-    password: ""
+    password: "",
   });
 
   const { isLoaded, signUp, setActive } = useSignUp();
 
   const [pendingVerification, setPendingVerification] = useState(false);
   const [code, setCode] = useState("");
+  const keyBoardState = useKeyboardState();
 
   // Handle submission of sign-up form
   const onSignUpPress = async () => {
     if (!isLoaded) return;
 
-		// Start sign-up process using email and password provided
-		try {
-			await signUp.create({
-				emailAddress: form.email,
-				password: form.password,
-				firstName: form.name.split(" ")[0],
-				lastName: form.name.split(" ")[1],
-			});
+    // Start sign-up process using email and password provided
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+        firstName: form.name.split(" ")[0],
+        lastName: form.name.split(" ")[1],
+      });
 
       // Send user an email with verification code
       await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
@@ -61,7 +63,12 @@ export default function Signup() {
 
         // --- ADDED NULL CHECK ---
         if (userId) {
-          await ensureJwt(userId,"signup",form.name.split(" ")[0],form.name.split(" ")[1]);
+          await ensureJwt(
+            userId,
+            "signup",
+            form.name.split(" ")[0],
+            form.name.split(" ")[1],
+          );
         }
         // ------------------------
 
@@ -78,54 +85,64 @@ export default function Signup() {
 
   if (pendingVerification) {
     return (
-      <>
-        <Text>Verify your email</Text>
-        <TextInput
-          value={code}
-          placeholder="Enter your verification code"
-          onChangeText={(code) => setCode(code)}
-        />
-        <TouchableOpacity onPress={onVerifyPress}>
-          <Text>Verify</Text>
-        </TouchableOpacity>
-      </>
+      <View
+        className={`flex-1 ${keyBoardState ? "justify-center" : "justify-end"}  items-center bg-green-800   w-[100%]`}
+      >
+        <View className="bg-white rounded-3xl w-full p-6 shadow-lg">
+          <Text>Verify your email</Text>
+          <TextInput
+            value={code}
+            placeholder="Enter your verification code"
+            onChangeText={(code) => setCode(code)}
+            textContentType="oneTimeCode"
+          />
+          <TouchableOpacity
+            className="bg-green-600 px-2 p-1"
+            onPress={onVerifyPress}
+          >
+            <Text>Verify</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 
-	return (
-		<View className="flex-1 bg-green-800 justify-end items-center  w-[100%]">
-			<View className="bg-white rounded-3xl w-full p-6 shadow-lg">
-				<Text className="text-2xl font-bold text-center mb-6">
-					Create Your Account
-				</Text>
+  return (
+    <View
+      className={`flex-1 ${keyBoardState ? "justify-center" : "justify-end"}  items-center bg-green-800   w-[100%]`}
+    >
+      <View className="bg-white rounded-3xl w-full p-6 shadow-lg">
+        <Text className="text-2xl font-bold text-center mb-6">
+          Create Your Account
+        </Text>
 
-				<TextInput
-					placeholder="Full Name"
-					value={form.name}
-					onChangeText={(t) => setForm({ ...form, name: t })}
-					className="border border-gray-300 rounded-xl px-4 py-3 mb-3"
-				/>
-				<TextInput
-					placeholder="Email"
-					keyboardType="email-address"
-					value={form.email}
-					onChangeText={(t) => setForm({ ...form, email: t })}
-					className="border border-gray-300 rounded-xl px-4 py-3 mb-3"
-				/>
-				<TextInput
-					placeholder="Password"
-					secureTextEntry
-					value={form.password}
-					onChangeText={(t) => setForm({ ...form, password: t })}
-					className="border border-gray-300 rounded-xl px-4 py-3 mb-3"
-				/>
+        <TextInput
+          placeholder="Full Name"
+          value={form.name}
+          onChangeText={(t) => setForm({ ...form, name: t })}
+          className="border border-gray-300 rounded-xl px-4 py-3 mb-3"
+        />
+        <TextInput
+          placeholder="Email"
+          keyboardType="email-address"
+          value={form.email}
+          onChangeText={(t) => setForm({ ...form, email: t })}
+          className="border border-gray-300 rounded-xl px-4 py-3 mb-3"
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry
+          value={form.password}
+          onChangeText={(t) => setForm({ ...form, password: t })}
+          className="border border-gray-300 rounded-xl px-4 py-3 mb-3"
+        />
 
-				<TouchableOpacity
-					className="bg-green-700 py-3 rounded-2xl mb-4"
-					onPress={onSignUpPress}
-				>
-					<Text className="text-center text-white font-semibold">Sign Up</Text>
-				</TouchableOpacity>
+        <TouchableOpacity
+          className="bg-green-700 py-3 rounded-2xl mb-4"
+          onPress={onSignUpPress}
+        >
+          <Text className="text-center text-white font-semibold">Sign Up</Text>
+        </TouchableOpacity>
 
         <Text className="text-center pb-6 text-gray-600">
           Already have an account?{" "}
